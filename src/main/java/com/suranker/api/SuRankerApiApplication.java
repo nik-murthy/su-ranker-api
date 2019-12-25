4e014abe08917dcc105fca2231ccb9c723b89dac
Your Name <nmurthy@icm-consulting.com.au>
2019-12-25 12:28:40 +1100

    Initial Commit

---
+++ src/main/java/com/suranker/api/SuRankerApiApplication.java
@@ -0,0 +1,13 @@
+package com.suranker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuRankerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuRankerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suranker/api/SuRankerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suranker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuRankerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
